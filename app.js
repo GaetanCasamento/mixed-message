9db3c0f83905a4e80011abee09d35b4b69c55587
Gaetan C <gaetan@gciweb.agency>
2022-09-30 12:49:40 +0100

    set up arrays of data for astro generator

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+//Define a random number to select a item in an array
 const randonNumbers = (length)=>{
     return Math.floor(Math.random()*length)
 };
+
+//set up different type of astro sentence generator
+const typeOfAstro = ['Amour', 'Argent', 'Travail'];
+
+
+//set up different sentence for each type of astro theme
+const astroLove = [
+'Le grand amour est pour bientôt, montrez un peu de patience',
+'La prochaine fois, réfléchissez à deux foix avant de rentrer dans l.e.a voisin.ine',
+'Si ça ne marche toujours pas, remetez-vous en question ou achètez-vous un chat',
+'Tous vas bien de votre couple, vous pouvez continuer à manger de la pizza devant la télé ou vous grattant les parties génitales'
+];
+const astroMoney = [
+''
+];
+const astroJob = [
+
+];
